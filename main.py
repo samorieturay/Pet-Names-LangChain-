7942e68ba2ef7934a3c06fb1d054f280c481283f
Sam Turay Jr <st3387@drexel.edu>
2024-08-22 01:39:44 -0400

    Had to switch to LangChain community edition

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 #Practicing LangChain with ChatGPT.
 
-from langchain.llms import openai #Choosing ChatGPT to be the large language model of our choosing
+from langchain_community.llms import OpenAI  # Correct import
 from dotenv import load_dotenv
-
+import os
 load_dotenv()
 
 def generate_pet_name():
-    llm = openai(temperature = 0.9) #temperature states how creative/accurate we want our responses to  be. 1 meaning super creative and 0 being as factual as possible
+    llm = OpenAI(temperature = 0.9) #temperature states how creative/accurate we want our responses to  be. 1 meaning super creative and 0 being as factual as possible
 
     name = llm("I have a pet cat and I want a cool name for it. Suggest me five cool names for my cat,") #this is my prompt
 
     return name
+
+if __name__ == "__main__":
+    pet_names = generate_pet_name()
+    print(pet_names)
